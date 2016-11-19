aa5f01c4f3a3d44d081aec73720b97022f16303a
mark-g-y <gye.2411@gmail.com>
2016-11-19 02:34:47 -0500

    Added image uploading on server side

--- web/jordanify/jordanify/controllers/api.py
+++ web/jordanify/jordanify/controllers/api.py
@@ -1,5 +1,15 @@
+import random
 from django.http import JsonResponse
 
 
 def jordanify_image(request):
-    return JsonResponse({'foo':'bar'})
+    save_file(request.FILES['image'])
+    return JsonResponse({'status':'success'})
+
+
+def save_file(file):
+    file_type = file.content_type.split('/')[1]
+    name = random.getrandbits(128)
+    with open('jordanify/static/uploads/' + str(name) + '.' + file_type, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
